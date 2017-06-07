2ff84bc2c0871785b643c6ae02c8987ed5d9cddc
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-06-07 14:24:16 +0900

    Update class 'SCPI'
    + compatible with UDP connection
    + compatible with Python 3.x

--- LMT/scpi.py
+++ LMT/scpi.py
@@ -2,11 +2,12 @@
 
 # standard library
 import sys
-from socket import socket, AF_INET, SOCK_STREAM
+import socket
+from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
 
 
 # classes
-class SCPI(object):
+class SCPI(socket.socket):
     """Create an interface to SCPI instruments.
 
     Args:
@@ -18,19 +19,51 @@ class SCPI(object):
         >>> FG = scpi.SCPI('192.168.1.2', port=8000)
         >>> FG.send('FREQ 10')
     """
-    def __init__(self, host, port=8000):
-        self.s = socket(AF_INET, SOCK_STREAM)
-        self.s.connect((host, port))
-        self.f = self.s.makefile('rb')
+    def __init__(self, host, port=8000, protocol='TCP'):
+        if protocol == 'TCP':
+            super(SCPI, self).__init__(AF_INET, SOCK_STREAM)
+            self.connect((host, port))
+        elif protocol == 'UDP':
+            super(SCPI, self).__init__(AF_INET, SOCK_DGRAM)
+        else:
+            raise ValueError(protocol)
 
-    def reset(self):
-        self.s.send('*RST\n')
-        self.s.send('*CLS\n')
+        self.info = {
+            'host': host,
+            'port': port,
+            'protocol': protocol,
+            'pythonver': sys.version_info.major
+        }
 
     def send(self, command):
-        print(command)
-        self.s.send('{0}\n'.format(command))
+        # make a sending data as bytes
+        if self.info['pythonver'] == 2:
+            senddata = '{0}\n'.format(command)
+        elif self.info['pythonver'] == 3:
+            senddata = '{0}\n'.format(command).encode('utf-8')
+
+        # send data
+        if self.info['protocol'] == 'TCP':
+            super(SCPI, self).send(senddata)
+        elif self.info['protocol'] == 'UDP':
+            address = (self.info['host'], self.info['port'])
+            super(SCPI, self).sendto(senddata, address)
+
+        # print status
+        print('SEND> {0}'.format(command))
+        if command.endswith('?') or (self.info['protocol'] == 'UDP'):
+            recvdata = self.get()
+            print('RECV> {0}'.format(recvdata))
 
     def get(self):
-        c = self.s.recv(1024)
-        print(c)
+        if self.info['pythonver'] == 2:
+            return self.recv(1024)
+        elif self.info['pythonver'] == 3:
+            return self.recv(1024).decode('utf-8')
+
+    def reset(self):
+        if self.info['protocol'] == 'UDP':
+            raise UserWarning('this method is for TCP only!')
+
+        self.send('*RST\n')
+        self.send('*CLS\n')
